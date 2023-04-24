b9eaa1fd726ba9c1dad14d2de865643d2fbb8d89
adairrr <32375605+adairrr@users.noreply.github.com>
2023-04-24 12:04:07 +0300

    Boot branch

--- contracts/external/cw-abc/src/boot.rs
+++ contracts/external/cw-abc/src/boot.rs
@@ -1,5 +1,5 @@
 use crate::msg::*;
-use boot_core::ContractWrapper;
+use boot_core::{ArtifactsDir, ContractWrapper, Daemon, Mock, TxHandler, Uploadable, WasmPath};
 use boot_core::{contract, Contract, CwEnv};
 
 #[contract(InstantiateMsg, ExecuteMsg, QueryMsg, Empty)]
@@ -8,15 +8,23 @@ pub struct CwAbc<Chain>;
 impl<Chain: CwEnv> CwAbc<Chain> {
     pub fn new(name: &str, chain: Chain) -> Self {
         let mut contract = Contract::new(name, chain);
-        contract = contract
-            .with_wasm_path("abstract_etf_app")
-            .with_mock(Box::new(
-                ContractWrapper::new(
-                    crate::contract::execute,
-                    crate::contract::instantiate,
-                    crate::contract::query,
-                )
-            ));
         Self(contract)
+    }
+}
+
+impl Uploadable<Mock> for CwAbc<Mock> {
+    fn source(&self) -> <Mock as TxHandler>::ContractSource {
+        let aoeu = ContractWrapper::new(
+            crate::contract::execute,
+            crate::contract::instantiate,
+            crate::contract::query,
+        );
+        Box::new(aoeu)
+    }
+}
+
+impl Uploadable<Daemon> for CwAbc<Daemon> {
+    fn source(&self) -> <Daemon as TxHandler>::ContractSource {
+        ArtifactsDir::env().unwrap().find_wasm_path("cw_abc").unwrap()
     }
 }
